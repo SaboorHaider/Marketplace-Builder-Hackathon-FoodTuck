791e4512a4a35a54e0cd8fd6a16764b0e7ed925d
Saboor_Haider <162649541+SaboorHaider@users.noreply.github.com>
2025-02-06 09:17:17 +0500

    Add files

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
-    images:{
-        remotePatterns:[
-          {
-            hostname:"cdn.sanity.io"
-          }
-        ]
-      }
-=======
-    images : {
-        domains :[
-            "images.pexels.com",
-            "cdn.sanity.io"
-        ],
-    },
->>>>>>> e9f6b11 (Update Files)
+  images : {
+      domains :[
+          "images.pexels.com",
+          "cdn.sanity.io"
+      ],
+  },
 };
 
 export default nextConfig;
